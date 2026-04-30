32303c52e88d53acf434e5c7b363b5d04c57ce71
Abhishek Arpure <aarpure@codeaurora.org>
2026-04-30 15:30:09 +0300

    universal7420-common: Move hardware specific .rc files to /vendor/etc/init/hw The reason these files are moved to /vendor/etc/init/hw and not /vendor/etc/init is because 'init' scans the /vendor/etc/init folder for vendor service .rc fragments to import. That would either result in duplicate imports or out-of-order imports of these hardware specific .rc files and that *may* have unintended consequences.
    In order to preserve the current import order for MTP, we move all
    hardware specific .rc files to /vendor/etc/init/hw which is not
    autoscanned by init BUT leave the init.${ro.hardware}.rc (i.e.
    init.qcom.rc in this case) in root for 'init' to find.
    
    Change-Id: I14531da05e2a3a256f734f8de96e1f1969c753f9

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -13,7 +13,7 @@ LOCAL_MODULE            := init.power.rc
 LOCAL_MODULE_TAGS       := optional
 LOCAL_MODULE_CLASS      := ETC
 LOCAL_SRC_FILES         := etc/init.power.rc
-LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -21,7 +21,7 @@ LOCAL_MODULE		:= init.samsungexynos7420.rc
 LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/init.samsungexynos7420.rc
-LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH	:= $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -29,7 +29,7 @@ LOCAL_MODULE		:= init.samsungexynos7420.usb.rc
 LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/init.samsungexynos7420.usb.rc
-LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH	:= $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
