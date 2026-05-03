ebd4def2fd57c664db5c29e0901284551b21f543
razorloves <razorloves@gmail.com>
2026-05-04 00:30:19 +0300

    universal7420-common: update dexpreopt option The recent change we made in the build repo makes all builds dexpreopt only boot img and system server. This changes angler builds back to full dexpreopt.
    Change-Id: I0cf03634ee7bbc93b18b49d3f1efe82a6302b447

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -96,6 +96,7 @@ TARGET_SOC := exynos7420
 # Dexpreopt
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
+    WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := false
     WITH_DEXPREOPT := true
   endif
 endif
