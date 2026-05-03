07f510c1cf757051f8de8376655c4e64a2a22462
Paul Keith <javelinanddart@gmail.com>
2026-05-04 00:28:32 +0300

    universal7420-common: Enable full dex preopt * So, uh, it turns out we need to disable booting with all   but 1 CPU core to fix CDMA RIL on klte (and hlte?), so   let's enable full dex preopt so that we don't need to   dex opt after system updates
    Change-Id: I6fe57f9dcd1da8245eebd51d61055d6de7ffe6cd

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -96,10 +96,7 @@ TARGET_SOC := exynos7420
 # Dexpreopt
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
-    endif
+    WITH_DEXPREOPT := true
   endif
 endif
 
