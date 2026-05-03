1ea63cbad1d0c7c5fe47c73ebeeed208ea9f44df
Salih Çağlar <salihcaglar4187@gmail.com>
2026-05-04 00:20:35 +0300

    universal7420-common: Include init.samsung.rc

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -17,6 +17,14 @@ LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE            := init.samsung.rc
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := etc/init.samsung.rc
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.samsungexynos7420.rc
 LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_CLASS	:= ETC
