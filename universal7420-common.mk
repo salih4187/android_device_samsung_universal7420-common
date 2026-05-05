3fb230f033c4e1a8d5f9fa99377e0d8e2f32679e
Salih Çağlar <salihcaglar4187@gmail.com>
2026-05-05 17:26:22 +0300

    universal7420-common: Build Configstore 1.1 HAL

--- universal7420-common.mk
+++ universal7420-common.mk
@@ -62,8 +62,8 @@ PRODUCT_PACKAGES += \
 
 # Configstore
 PRODUCT_PACKAGES += \
-    android.hardware.configstore@1.0-impl \
-    android.hardware.configstore@1.0-service
+    android.hardware.configstore@1.1-impl \
+    android.hardware.configstore@1.1-service
 
 # DRM
 PRODUCT_PACKAGES += \
