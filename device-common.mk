d6f1c6f6879419845e564dd685db49db7b99d3c0
Salih Çağlar <salihcaglar4187@gmail.com>
2026-05-06 10:54:58 +0300

    universal7420-common: gnss: Rename it to universal7420

--- device-common.mk
+++ device-common.mk
@@ -76,8 +76,8 @@ PRODUCT_PACKAGES += \
 
 # GPS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl \
-    android.hardware.gnss@1.0-service
+    android.hardware.gnss@1.0-impl.universal7420 \
+    android.hardware.gnss@1.0-service.universal7420
 
 # Configs
 PRODUCT_COPY_FILES += \
